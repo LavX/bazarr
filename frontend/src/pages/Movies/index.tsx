02113e2face286c1d3d7c3d9ee0e0e5b683919bc
Laszlo Toth <laszlo.toth@baxter-it.com>
2026-03-25 14:15:28 +0100

    feat: add inline bulk actions to Movies page
    Add row selection checkboxes and a toolbar with Sync Subtitles,
    Subtitle Tools, Translate, Scan Disk, and Search Missing actions.

--- frontend/src/pages/Movies/index.tsx
+++ frontend/src/pages/Movies/index.tsx
@@ -1,21 +1,33 @@
 import { FunctionComponent, useMemo, useState } from "react";
 import { Link } from "react-router";
-import { Anchor, Badge, Container, Tooltip } from "@mantine/core";
+import { Anchor, Badge, Checkbox, Container, Group, Menu, Tooltip } from "@mantine/core";
 import { useDocumentTitle } from "@mantine/hooks";
 import { faBookmark as farBookmark } from "@fortawesome/free-regular-svg-icons";
-import { faBookmark, faWrench } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBookmark,
+  faHardDrive,
+  faLanguage,
+  faMagnifyingGlass,
+  faSync,
+  faToolbox,
+  faWrench,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { ColumnDef } from "@tanstack/react-table";
 import { uniqueId } from "lodash";
 import { useMovieModification, useMoviesPagination } from "@/apis/hooks";
 import { useInstanceName } from "@/apis/hooks/site";
-import { Action } from "@/components";
+import { Action, Toolbox } from "@/components";
 import { AudioList } from "@/components/bazarr";
 import Language from "@/components/bazarr/Language";
 import LanguageProfileName from "@/components/bazarr/LanguageProfile";
+import { BatchModConfirmModal } from "@/components/forms/BatchModConfirmForm";
 import { ItemEditModal } from "@/components/forms/ItemEditForm";
+import { MassSyncModal } from "@/components/forms/MassSyncForm";
+import { MassTranslateModal } from "@/components/forms/MassTranslateForm";
 import { useModals } from "@/modules/modals";
 import ItemView from "@/pages/views/ItemView";
+import { BatchAction, BatchItem } from "@/apis/raw/subtitles";
 import { BuildKey } from "@/utilities";
 
 const MovieView: FunctionComponent = () => {
@@ -33,8 +45,28 @@ const MovieView: FunctionComponent = () => {
 
   const query = useMoviesPagination(hasActiveFilter);
 
+  const [selections, setSelections] = useState<Item.Movie[]>([]);
+
   const columns = useMemo<ColumnDef<Item.Movie>[]>(
     () => [
+      {
+        id: "selection",
+        header: ({ table }) => (
+          <Checkbox
+            id="movies-select-all"
+            indeterminate={table.getIsSomeRowsSelected()}
+            checked={table.getIsAllRowsSelected()}
+            onChange={table.getToggleAllRowsSelectedHandler()}
+          />
+        ),
+        cell: ({ row }) => (
+          <Checkbox
+            id={`movies-select-${row.index}`}
+            checked={row.getIsSelected()}
+            onChange={row.getToggleSelectedHandler()}
+          />
+        ),
+      },
       {
         id: "monitored",
         cell: ({
@@ -143,6 +175,96 @@ const MovieView: FunctionComponent = () => {
     [modals, modifyMovie],
   );
 
+  const selectionToolbar = useMemo(() => {
+    if (selections.length === 0) return undefined;
+
+    const toBatchItems = (): BatchItem[] =>
+      selections.map((m) => ({
+        type: "movie" as const,
+        radarrId: m.radarrId,
+      }));
+
+    return (
+      <Group gap="xs">
+        <Toolbox.Button
+          icon={faSync}
+          onClick={() =>
+            modals.openContextModal(MassSyncModal, { items: toBatchItems() })
+          }
+        >
+          Sync Subtitles
+        </Toolbox.Button>
+
+        <Menu shadow="md" width={220}>
+          <Menu.Target>
+            <div>
+              <Toolbox.Button icon={faToolbox}>Subtitle Tools</Toolbox.Button>
+            </div>
+          </Menu.Target>
+          <Menu.Dropdown>
+            {(
+              [
+                ["OCR_fixes", "OCR Fixes"],
+                ["common", "Common Fixes"],
+                ["remove_HI", "Remove Hearing Impaired"],
+                ["remove_tags", "Remove Style Tags"],
+                ["fix_uppercase", "Fix Uppercase"],
+                ["reverse_rtl", "Reverse RTL"],
+              ] as [BatchAction, string][]
+            ).map(([action, label]) => (
+              <Menu.Item
+                key={action}
+                onClick={() =>
+                  modals.openContextModal(BatchModConfirmModal, {
+                    items: toBatchItems(),
+                    action,
+                  })
+                }
+              >
+                {label}
+              </Menu.Item>
+            ))}
+          </Menu.Dropdown>
+        </Menu>
+
+        <Toolbox.Button
+          icon={faLanguage}
+          onClick={() =>
+            modals.openContextModal(MassTranslateModal, {
+              items: toBatchItems(),
+            })
+          }
+        >
+          Translate
+        </Toolbox.Button>
+
+        <Toolbox.Button
+          icon={faHardDrive}
+          onClick={() =>
+            modals.openContextModal(BatchModConfirmModal, {
+              items: toBatchItems(),
+              action: "scan-disk" as BatchAction,
+            })
+          }
+        >
+          Scan Disk
+        </Toolbox.Button>
+
+        <Toolbox.Button
+          icon={faMagnifyingGlass}
+          onClick={() =>
+            modals.openContextModal(BatchModConfirmModal, {
+              items: toBatchItems(),
+              action: "search-missing" as BatchAction,
+            })
+          }
+        >
+          Search Missing
+        </Toolbox.Button>
+      </Group>
+    );
+  }, [selections, modals]);
+
   useDocumentTitle(`Movies - ${useInstanceName()}`);
 
   return (
@@ -156,6 +278,9 @@ const MovieView: FunctionComponent = () => {
         onAudioLanguagesChange={setAudioLanguages}
         excludeLanguages={excludeLanguages}
         onExcludeLanguagesChange={setExcludeLanguages}
+        enableRowSelection
+        onSelectionChanged={setSelections}
+        selectionToolbar={selectionToolbar}
       ></ItemView>
     </Container>
   );
